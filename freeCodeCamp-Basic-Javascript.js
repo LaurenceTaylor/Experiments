22199d7e8b02b40ce73d356cc07dd734b053a8e5
Laurence <ltaylorapp@gmail.com>
2017-09-10 22:21:12 +0100

    Update freeCodeCamp-Basic-Javascript.js

--- freeCodeCamp-Basic-Javascript.js
+++ freeCodeCamp-Basic-Javascript.js
@@ -11,8 +11,11 @@ myVar--;
 myVar = myVar - 1;
 
 // % divides and gives the remainder
-// 5 % 2 = 1
-// 17 % 2 = 1 (17 is Odd)
-// 48 % 2 = 0 (48 is Even)
+// 17 % 2 = 1 (17 is Odd); 48 % 2 = 0 (48 is Even)
 myVar = 11 % 3;
+
+// use += -= *= /= to quickly reassign the value of a variable
+myVar += 3;
+// is the same as
+myVar = myVar + 3;
 
