f0918cbaf3a09e329a9220cf0d7b9eff60b0638b
Laurence <ltaylorapp@gmail.com>
2017-09-10 22:16:19 +0100

    Update freeCodeCamp-Basic-Javascript.js

--- freeCodeCamp-Basic-Javascript.js
+++ freeCodeCamp-Basic-Javascript.js
@@ -1,1 +1,18 @@
+#########################################################################################
+// Dealing with numbers
+#########################################################################################
+
+myVar++;
+// this is equivalent to:
+myVar = myVar + 1;
+
+myVar--;
+// this is equivalent to:
+myVar = myVar - 1;
+
+// % divides and gives the remainder
+// 5 % 2 = 1
+// 17 % 2 = 1 (17 is Odd)
+// 48 % 2 = 0 (48 is Even)
+myVar = 11 % 3;
 
