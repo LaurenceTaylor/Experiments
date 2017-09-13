b43131c8f0b481b37d8a920b3ceea25aa86ef551
Laurence <ltaylorapp@gmail.com>
2017-09-13 17:12:52 +0100

    Update freeCodeCamp-Basic-Javascript.js

--- freeCodeCamp-Basic-Javascript.js
+++ freeCodeCamp-Basic-Javascript.js
@@ -134,3 +134,25 @@ return "Inside";
 // Switches
 #########################################################################################
 
+// Used to test many options with strict equality
+// example
+function caseInSwitch(val) {
+  var answer = "";
+  switch (val) {
+    case 1:
+      answer = "alpha";
+      break;
+    case 2:
+      answer = "beta";
+      break;
+    case 3:
+      answer = "gamma";
+      break;
+    case 4:
+      answer = "delta";
+      break;
+  } 
+  return answer;  
+}
+
+caseInSwitch(3);
