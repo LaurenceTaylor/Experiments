22ba40b86b4ebc51f73d05989655a245b35601e9
Laurence <ltaylorapp@gmail.com>
2017-09-26 22:43:45 +0100

    Update freeCodeCamp-Alg-Scripting.js

--- freeCodeCamp-Alg-Scripting.js
+++ freeCodeCamp-Alg-Scripting.js
@@ -2,3 +2,11 @@
 // Reverse a String
 /////////////////////////////////////////////////////////////////////////////////////////
 
+function reverseString(str) {
+  str = str.split('');            // split the string into an array, no delimiter
+  str = str.reverse();            // reverse the array
+  str = str.join('');             // join the array back into a string, no delimiter
+  return str;
+}
+
+reverseString("Greetings from Earth");
