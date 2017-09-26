7621a967c0a40c0953508f3a774c9c6f56f5ece2
Laurence <ltaylorapp@gmail.com>
2017-09-26 23:05:49 +0100

    Update freeCodeCamp-Alg-Scripting.js

--- freeCodeCamp-Alg-Scripting.js
+++ freeCodeCamp-Alg-Scripting.js
@@ -10,3 +10,32 @@ function reverseString(str) {
 }
 
 reverseString("Greetings from Earth");
+
+////////////////////
+// Simpler solution:
+
+function reverseString(str) {
+  return str.split('').reverse().join('');
+}
+
+/////////////////////////////////////////////////////////////////////////////////////////
+// Factorialize a Number
+/////////////////////////////////////////////////////////////////////////////////////////
+
+function factorialize(num) {
+  var total = 1;                  // set total outside of loop, starting at 1 because we're multiplying
+  for (i = 1; i <= num; i++) {    // set up for loop, with i running from 1 to num
+    total *= i;                   // multiply running total by i each iteration
+  }
+  return total;
+}
+
+factorialize(5);
+
+////////////////////
+// Simpler solution:
+
+function factorialize(num) {          // this solutions uses recursion instead
+  if (num === 0) { return 1; }
+  return num * factorialize(num-1);
+}
