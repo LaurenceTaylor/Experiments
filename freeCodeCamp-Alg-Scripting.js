7122dd3436a50858f2ff4fd13cc5bbc4a2d06c14
Laurence <ltaylorapp@gmail.com>
2017-09-27 15:58:32 +0100

    Update freeCodeCamp-Alg-Scripting.js

--- freeCodeCamp-Alg-Scripting.js
+++ freeCodeCamp-Alg-Scripting.js
@@ -52,3 +52,11 @@ function palindrome(str) {
 }
 
 palindrome("eye");
+
+////////////////////
+// Simpler solution:
+
+function palindrome(str) {
+  return str.replace(/[\W_]/g, '').toLowerCase() ===
+         str.replace(/[\W_]/g, '').toLowerCase().split('').reverse().join('');
+}
