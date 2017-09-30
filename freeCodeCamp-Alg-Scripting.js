38cca02e8f5bf8325d206e98561da9389f6629f5
Laurence <ltaylorapp@gmail.com>
2017-09-30 18:33:50 +0100

    Update freeCodeCamp-Alg-Scripting.js

--- freeCodeCamp-Alg-Scripting.js
+++ freeCodeCamp-Alg-Scripting.js
@@ -112,3 +112,13 @@ function largestOfFour(arr) {
 }
 
 largestOfFour([[4, 5, 1, 3], [13, 27, 18, 26], [32, 35, 37, 39], [1000, 1001, 857, 1]]);
+
+/////////////////////////////////////////////////////////////////////////////////////////
+// Confirm the Ending
+/////////////////////////////////////////////////////////////////////////////////////////
+
+function confirmEnding(str, target) {
+  return str.substr(- target.length) === target;
+}
+
+confirmEnding("Bastian", "n");
