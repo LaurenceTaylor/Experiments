ad9db03f5dae881c498027c94386ef36aaff5806
Laurence <ltaylorapp@gmail.com>
2017-10-02 12:35:58 +0100

    Update freeCodeCamp-Alg-Scripting.js

--- freeCodeCamp-Alg-Scripting.js
+++ freeCodeCamp-Alg-Scripting.js
@@ -187,3 +187,13 @@ function truncateString(str, num) {
 // Chunky Monkey
 /////////////////////////////////////////////////////////////////////////////////////////
 
+function chunkArrayInGroups(arr, size) {
+  var newArray = [];
+  for (var i = 0; i < arr.length; i += size) {
+    var tempvar = arr.slice(i, i + size);
+    newArray.push(tempvar);
+  }
+  return newArray;
+}
+
+chunkArrayInGroups(["a", "b", "c", "d"], 2);
