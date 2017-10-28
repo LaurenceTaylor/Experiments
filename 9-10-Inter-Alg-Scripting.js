f4d51e7fa932543ee2f8c1830ede2f2898a9b430
Laurence <30178403+LaurenceTaylor@users.noreply.github.com>
2017-10-28 13:18:06 +0100

    Update 9-10-Inter-Alg-Scripting.js

--- 9-10-Inter-Alg-Scripting.js
+++ 9-10-Inter-Alg-Scripting.js
@@ -4,8 +4,8 @@
 
 function sumAll(arr) {
   var total = 0;
-  for (var i = Math.min(arr[0], arr[1]); i <= Math.max(arr[0], arr[1]); i++) {
-    total += i;
+  for (var i = Math.min(arr[0], arr[1]); i <= Math.max(arr[0], arr[1]); i++) {    // range from lowest to highest number in arr
+    total += i;                                                                   // add i to total each iteration
   }
   return total;
 }
@@ -15,7 +15,7 @@ function sumAll(arr) {
 
 function sumAll(arr) {
     var sum = 0;
-    for (var i = Math.min(...arr); i <= Math.max(...arr); i++){
+    for (var i = Math.min(...arr); i <= Math.max(...arr); i++){                   // similar solution, with spread operator ...arr
         sum += i;
     }
   return sum;
