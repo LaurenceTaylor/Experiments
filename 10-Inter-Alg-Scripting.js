b3433a6ad5f7fcc72d817b9da47fb81637e773f1
Laurence <30178403+LaurenceTaylor@users.noreply.github.com>
2017-10-28 11:28:26 +0100

    Update 10-Inter-Alg-Scripting.js

--- 10-Inter-Alg-Scripting.js
+++ 10-Inter-Alg-Scripting.js
@@ -1,1 +1,22 @@
+/////////////////////////////////////////////////////////////////////////////////////////
+// Sum All Numbers in a Range
+/////////////////////////////////////////////////////////////////////////////////////////
 
+function sumAll(arr) {
+  var total = 0;
+  for (var i = Math.min(arr[0], arr[1]); i <= Math.max(arr[0], arr[1]); i++) {
+    total += i;
+  }
+  return total;
+}
+
+////////////////////
+// Advanced solution:
+
+function sumAll(arr) {
+    var sum = 0;
+    for (var i = Math.min(...arr); i <= Math.max(...arr); i++){
+        sum += i;
+    }
+  return sum;
+}
